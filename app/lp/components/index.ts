3044ccd571251820889ebefd916077afa34f9976
Joel Hinton <hintonjoel@gmail.com>
2026-01-19 20:57:36 -0500

    feat(lp): add component index exports

---
+++ app/lp/components/index.ts
@@ -0,0 +1,7 @@
+export { HeroSection } from './HeroSection';
+export { MultiStepForm } from './MultiStepForm';
+export { SocialProof } from './SocialProof';
+export { PainPoints } from './PainPoints';
+export { BenefitsGrid, seoAuditBenefits } from './BenefitsGrid';
+export { FAQAccordion, seoAuditFAQs } from './FAQAccordion';
+export { StickyFooterCTA } from './StickyFooterCTA';
